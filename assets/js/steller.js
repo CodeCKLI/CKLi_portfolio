6281e2597bb75f45c3f065588db5775d23930adb
Mattzac <hk.mattli@gmail.com>
2023-12-17 18:26:21 -0500

    fixed layouts

---
+++ assets/js/steller.js
@@ -0,0 +1,43 @@
+/*!
+=========================================================
+* Steller Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Send email with message form
+$(document).ready(function () {
+  $("#btnSubmit").click(function () {
+    alert(
+      "Thank you so much for reaching out! Please wait for the request to go through"
+    );
+  });
+});
+
+// smooth scroll
+$(document).ready(function () {
+  $(".nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
